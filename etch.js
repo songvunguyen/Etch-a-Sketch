b89adece24151e34c933641b3d66d68114a60a5f
Song Vu Nguyen <vunnems08@gmail.com>
2020-12-03 12:28:13 -0500

    Center the grid

--- etch.js
+++ etch.js
@@ -7,7 +7,7 @@ for(let i = 0; i < 16; i++){
         div.classList.add("grid_cell");
         div.setAttribute("id", i.toString()+"_"+j.toString());
         div.style.cssText = `grid-column: ${i+1} / ${i+2}; grid-row: ${j+1} / ${j+2}; border-style: solid;
-        background-color: green`;
+        background-color: white`;
         container.appendChild(div);
     }
 }
